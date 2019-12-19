710533c432d370547d6d735bf5424f7d97033ab3
Ricardo Persaud <ricardo39985@gmail.com>
2019-12-19 16:14:50 +0000

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,30 @@
+class Song
+  attr_reader :name, :artist, :genre
+  @@count = 0
+  @@genres = []
+  @@artists = []
+  def initialize(name, artist, genre)
+    @name=name
+    @artist = artist
+    @genre = genre
+    @@count+=1
+    @@genres << genre
+    @@artists << artist
+
+  end
+  def self.count
+    @@count
+  end
+  def self.genres
+    @@genres.uniq
+  end
+  def self.artists
+    @@artists.uniq
+  end
+  def self.genre_count
+    Hash[@@genres.collect { |item| [item, @@genres.count(item)] }]
+  end
+  def self.artist_count
+    Hash[@@artists.map{|artist| [artist, @@artists.count(artist)]}]
+  end
+end
